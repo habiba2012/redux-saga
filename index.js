e997a3f8920ab41587022a8de58925df513b6872
habiba2012 <habibaakhter2012@gmail.com>
2021-02-11 22:49:41 +0100

    'addReducer'

---
+++ index.js
@@ -0,0 +1,54 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const BUY_CAKE = "BUY_CAKE";
+const BUY_ICECERAM ="BUY_ICECREAM";
+
+function buyCake() {
+  return {
+    type: BUY_CAKE,
+    info: "First redux action",
+  };
+}
+
+function buyIceCream(){
+  return{
+    type: BUY_ICECERAM 
+
+  }
+}
+
+//(previousState, action) => newState
+const initialState = {
+  numOfCakes: 10,
+  numOfIceCreams: 20
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    case BUY_ICECERAM:return {
+      ...state,
+      numOfIceCreams: state.numOfIceCreams -1 
+    }  
+    default: return state;
+  }
+};
+
+const store = createStore(reducer);
+console.log("initial state", store.getState());
+const unsubscribe = store.subscribe(() =>
+  console.log("updated state", store.getState())
+);
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyCake());
+store.dispatch(buyIceCream());
+store.dispatch(buyIceCream());
+
+unsubscribe();
